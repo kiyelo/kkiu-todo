94d61e9c15d6ce9a5ebb5aa9173de9c250bca45a
kiyelo <yeonglong.k@gmail.com>
2026-07-29 19:43:56 +0900

    revert: restore pre-HIG app entry

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,14 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 
-const designMode = new URLSearchParams(window.location.search).get('design')
-
-if (designMode !== 'original') {
-  await import('./hig-overrides.css')
-  const { installHigEnhancements } = await import('./higEnhancements.js')
-  installHigEnhancements()
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>,
 )
