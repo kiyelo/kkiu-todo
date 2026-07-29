6e166a38190fc5f3c30bab6e3582dd03cf02ec09
kiyelo <yeonglong.k@gmail.com>
2026-07-29 19:40:09 +0900

    feat: add original/current design comparison mode

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
-import './hig-overrides.css'
-import { installHigEnhancements } from './higEnhancements.js'
 
-installHigEnhancements()
+const designMode = new URLSearchParams(window.location.search).get('design')
+
+if (designMode !== 'original') {
+  await import('./hig-overrides.css')
+  const { installHigEnhancements } = await import('./higEnhancements.js')
+  installHigEnhancements()
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>,
