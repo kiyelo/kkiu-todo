fbd775dd2181704c2c524cf4b301e0b5b7c4e5c8
kiyelo <yeonglong.k@gmail.com>
2026-08-14 23:27:32 +0900

    Use shared success highlight styles

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -7,6 +7,7 @@ import './styles.css'
 import './queuePerformance.css'
 import './queueNativeScroll.css'
 import './reorderFix.css'
+import './successHighlight.css'
 import './reorderDropFlip.js'
 
 restoreStartupTabForExistingSession()
