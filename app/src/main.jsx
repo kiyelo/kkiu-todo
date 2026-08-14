2bec8b257a199b738666e15eff7e600a947bf55b
kiyelo <yeonglong.k@gmail.com>
2026-08-14 20:27:10 +0900

    Remove obsolete composer hit-layer bridge

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -6,7 +6,6 @@ import { restoreStartupTabForExistingSession } from './services/startupTab.js'
 import './styles.css'
 import './queuePerformance.css'
 import './composerNativeScroll.css'
-import './composerNativeScroll.js'
 import './reorderFix.css'
 import './reorderDropFlip.js'
 
