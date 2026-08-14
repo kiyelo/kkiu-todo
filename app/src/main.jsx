bbc852ac3feb8f8f678bafd694ad6292ee91ca08
kiyelo <yeonglong.k@gmail.com>
2026-08-14 23:40:56 +0900

    Use single runtime stylesheet entrypoint

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { initializeNativeAuth } from './services/nativeAuth.js'
 import { restoreStartupTabForExistingSession } from './services/startupTab.js'
-import './styles.css'
-import './styles/queue.css'
-import './styles/taskHighlight.css'
+import './styles/index.css'
 import './interactions/reorderHighlight.js'
 
 restoreStartupTabForExistingSession()
