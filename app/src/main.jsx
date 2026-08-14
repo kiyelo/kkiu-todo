6b904c7ed08845b79d0d3c7edec8831cf9c81498
kiyelo <yeonglong.k@gmail.com>
2026-08-14 23:13:48 +0900

    Remove reorder drop animation

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -7,7 +7,6 @@ import './styles.css'
 import './queuePerformance.css'
 import './queueNativeScroll.css'
 import './reorderFix.css'
-import './reorderDropFlip.js'
 
 restoreStartupTabForExistingSession()
 void initializeNativeAuth()
