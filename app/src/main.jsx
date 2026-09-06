27a01938b6c1a8568aa6bd7aca0c05cf1730421d
kiyelo <yeonglong.k@gmail.com>
2026-09-06 21:56:54 +0900

    Load task swipe styles

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { initializeNativeAuth } from './services/nativeAuth.js'
 import './styles/index.css'
+import './styles/taskSwipe.css'
 import './interactions/reorderHighlight.js'
 
 await initializeNativeAuth()
